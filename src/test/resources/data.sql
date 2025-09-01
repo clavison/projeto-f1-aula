50e26ae16a8c45e4cec20fd5fbf1f6fb28bcd485
Clavison Zapelini <clavison.zapelini@attus.ai>
2025-09-01 15:58:55 -0300

    Configuração application-test.properties e data.sql

---
+++ src/test/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO pais (id, nome, sigla) VALUES (1, 'Brasil', 'BR');
+INSERT INTO pais (id, nome, sigla) VALUES (2, 'Itália', 'IT');
+
+INSERT INTO piloto (id, nome, idade, pais_id) VALUES (1, 'Ayrton Senna', 34, 1);
+INSERT INTO piloto (id, nome, idade, pais_id) VALUES (2, 'Michael Schumacher', 35, 2);
